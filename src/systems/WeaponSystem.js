0015658a5a8b634c4f9ce7b7a1628d44baf95f31
carpet1994 <94118860+carpet1994@users.noreply.github.com>
2026-06-23 19:34:35 +0200

    Refactor WeaponManager to use namespaced imports

--- src/systems/WeaponSystem.js
+++ src/systems/WeaponSystem.js
@@ -1,35 +1,36 @@
-import { ArcaneBolt, OrbitingBlade, HolyPulse, LightningMark } from '../weapons/WeaponTypes.js';
+import * as W from '../weapons/WeaponTypes.js';
+import { EVOLUTION_DATA } from '../config/EvolutionConfig.js';
 
 export default class WeaponManager {
-    constructor() {
-        this.weapons = [new ArcaneBolt()];
-    }
-
-    update(dt, player, enemies) {
-        this.weapons.forEach(w => w.update(dt, player, enemies));
-    }
-
-    draw(ctx, camera) {
-        this.weapons.forEach(w => w.draw(ctx, camera));
-    }
+    constructor() { this.weapons = [new W.ArcaneBolt()]; }
+    update(dt, player, enemies) { this.weapons.forEach(w => w.update(dt, player, enemies)); }
+    draw(ctx, camera) { this.weapons.forEach(w => w.draw(ctx, camera)); }
 
     addOrUpgradeWeapon(id) {
         const existing = this.weapons.find(w => w.id === id);
-        if (existing) {
-            existing.levelUp();
-        } else {
-            const newWeapon = this.createNewWeapon(id);
-            if (newWeapon) this.weapons.push(newWeapon);
+        if (existing) existing.levelUp();
+        else this.weapons.push(this.createNewWeapon(id));
+    }
+
+    evolveWeapon(id) {
+        const index = this.weapons.findIndex(w => w.id === id);
+        const config = EVOLUTION_DATA[id];
+        if (index !== -1 && config) {
+            this.weapons[index] = this.createNewWeapon(config.result);
         }
     }
 
     createNewWeapon(id) {
         switch(id) {
-            case 'bolt': return new ArcaneBolt();
-            case 'orbit': return new OrbitingBlade();
-            case 'pulse': return new HolyPulse();
-            case 'light': return new LightningMark();
+            case 'bolt': return new W.ArcaneBolt();
+            case 'storm': return new W.ArcaneStorm();
+            case 'orbit': return new W.OrbitingBlade();
+            case 'celestial': return new W.CelestialBlades();
+            case 'pulse': return new W.HolyPulse();
+            case 'nova': return new W.DivineNova();
+            case 'light': return new W.LightningMark();
+            case 'crown': return new W.ThunderCrown();
             default: return null;
         }
     }
-}
+    }
