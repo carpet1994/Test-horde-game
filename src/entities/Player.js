7208ec6e5d047bfac000f4ebc4b62f248ad95353
carpet1994 <94118860+carpet1994@users.noreply.github.com>
2026-06-23 18:27:48 +0200

    Adjust player speed and update movement controls

--- src/entities/Player.js
+++ src/entities/Player.js
@@ -1,15 +1,16 @@
 export default class Player {
     constructor(x, y) {
-        this.x = x; this.y = y; this.size = 182; this.speed = 300;
+        this.x = x; this.y = y; this.size = 182; this.speed = 400;
+        this.lastShot = 0;
     }
     update(dt, input) {
-        if (input.keys['w']) this.y -= this.speed * dt;
-        if (input.keys['s']) this.y += this.speed * dt;
-        if (input.keys['a']) this.x -= this.speed * dt;
-        if (input.keys['d']) this.x += this.speed * dt;
+        if (input.keys['w'] || input.keys['ArrowUp']) this.y -= this.speed * dt;
+        if (input.keys['s'] || input.keys['ArrowDown']) this.y += this.speed * dt;
+        if (input.keys['a'] || input.keys['ArrowLeft']) this.x -= this.speed * dt;
+        if (input.keys['d'] || input.keys['ArrowRight']) this.x += this.speed * dt;
     }
-    draw(ctx) {
+    draw(ctx, camera) {
         ctx.fillStyle = '#00ffcc';
-        ctx.fillRect(this.x - this.size/2, this.y - this.size/2, this.size, this.size);
+        ctx.fillRect(this.x - camera.x - this.size/4, this.y - camera.y - this.size/4, this.size/2, this.size/2);
     }
 }
