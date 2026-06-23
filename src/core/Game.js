4328d1c0814b77999e2a6eeb04bdbb19beffcead
carpet1994 <94118860+carpet1994@users.noreply.github.com>
2026-06-23 19:49:44 +0200

    Refactor Game class and add game loop functionality

--- src/core/Game.js
+++ src/core/Game.js
@@ -80,27 +80,63 @@ export default class Game {
             }
         });
     }
+import { GameConfig } from '../config/GameConfig.js';
+import { EVOLUTION_DATA } from '../config/EvolutionConfig.js';
+import { StorageManager } from './StorageManager.js';
+import Player from '../entities/Player.js';
+import Input from './Input.js';
+import WeaponManager from '../systems/WeaponSystem.js';
+import Spawner from '../systems/Spawner.js';
+import CollisionSystem from '../systems/CollisionSystem.js';
+import Map from './Map.js';
+import { getChoices } from '../systems/UpgradeManager.js';
 
-    triggerLevelUp() {
-        this.isLevelingUp = true;
-        this.currentChoices = getChoices(this.player, this.weaponManager);
-    }
+export default class Game {
+    constructor(canvasId, onGameOver) {
+        this.canvas = document.getElementById(canvasId);
+        this.ctx = this.canvas.getContext('2d');
+        this.canvas.width = GameConfig.resolution.width;
+        this.canvas.height = GameConfig.resolution.height;
 
-    triggerChestReward() {
-        this.isChestState = true;
-        this.evolutionAvailable = null;
-        for (const [id, data] of Object.entries(EVOLUTION_DATA)) {
-            const w = this.weaponManager.weapons.find(wp => wp.id === id);
-            if (w && w.level === 8 && this.player.passives[data.passive] >= 1) {
-                this.evolutionAvailable = id;
-                break;
-            }
-        }
-    }
-
-    handleChestClaim() {
-        if (this.evolutionAvailable) this.weaponManager.evolveWeapon(this.evolutionAvailable);
+        this.input = new Input();
+        this.player = new Player(0, 0);
+        this.map = new Map(5000, 5000);
+        this.weaponManager = new WeaponManager();
+        this.spawner = new Spawner();
+        this.onGameOver = onGameOver;
+        
+        this.enemies = [];
+        this.gems = [];
+        this.chests = [];
+        this.runStats = { time: 0, kills: 0, bosses: 0, earnedCoins: 0 };
+        
+        this.survivalTimer = 0;
+        this.lastTime = 0;
+        this.running = true;
+        this.isLevelingUp = false;
         this.isChestState = false;
+        this.gameOver = false;
+        this.currentChoices = [];
+        this.evolutionAvailable = null;
+    }
+
+    start() { requestAnimationFrame(this.loop.bind(this)); }
+
+    loop(timestamp) {
+        let dt = Math.min((timestamp - this.lastTime) / 1000, 0.1);
+        this.lastTime = timestamp;
+        if (this.running && !this.isLevelingUp && !this.isChestState) this.update(dt);
+        this.draw();
+        if (this.running) requestAnimationFrame(this.loop.bind(this));
+    }
+
+    update(dt) {
+        this.survivalTimer += dt;
+        this.player.update(dt, this.input);
+        this.spawner.update(dt, this.enemies, this.player, this.survivalTimer);
+        this.weaponManager.update(dt, this.player, this.enemies);
+        CollisionSystem.update(this);
+        if (this.player.hp <= 0) this.triggerGameOver();
     }
 
     triggerGameOver() {
@@ -120,25 +156,19 @@ export default class Game {
 
         this.ctx.fillStyle = '#000';
         this.ctx.fillRect(0, 0, 1920, 1080);
-
+        
+        this.map.draw(this.ctx, camera);
         this.player.draw(this.ctx, camera);
         this.enemies.forEach(e => e.draw(this.ctx, camera));
         this.gems.forEach(g => g.draw(this.ctx, camera));
         this.chests.forEach(c => c.draw(this.ctx, camera));
         this.weaponManager.draw(this.ctx, camera);
         
-        this.drawUI();
         if (this.isLevelingUp) this.drawLevelUpMenu();
         if (this.isChestState) this.drawChestScreen();
         if (this.gameOver) this.drawGameOver();
     }
-
-    drawUI() {
-        this.ctx.fillStyle = 'white';
-        this.ctx.font = '30px Arial';
-        this.ctx.fillText(`Coins: ${this.runStats.earnedCoins}`, 1700, 50);
-        this.ctx.fillText(`Time: ${Math.floor(this.survivalTimer)}s`, 50, 50);
-    }
+    
 
     drawLevelUpMenu() {
         this.ctx.fillStyle = 'rgba(0,0,0,0.9)';
